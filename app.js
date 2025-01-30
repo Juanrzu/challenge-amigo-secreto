830eb85bf2261739a69fffaa3466051549b88460
Juan Rodriguez <juanrzu.16@gmail.com>
2025-01-29 20:10:14 -0400

    funcion_agregar_amigos

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+
+//Almacenar amigos ingresados
+let amigos =[];
+
+
+
+// Crear funcion para agregar amigos
+function agregarAmigo(){ 
+    if (document.getElementById('amigo').value == ''){
+        alert("el campo no puede estar vacio");
+        
+    }else{
+        //
+        let agregarAmigos= amigos.push(document.getElementById('amigo').value);    
+            lista();
+            limpiarCampo();
+            return;
+        }  
+    }
+   
+   
